56912dd273943e6d8dff251d90e5f5fc80ef563d
Ahadvt <ahadvt@code.edu.az>
2022-07-17 15:55:57 +0400

    lab

---
+++ P127_Pronia/P127_Pronia/Controllers/PlantController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using P127_Pronia.DAL;
+using P127_Pronia.Models;
+using P127_Pronia.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P127_Pronia.Controllers
+{
+    public class PlantController:Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlantController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if(id is null || id == 0)
+            {
+                return NotFound();
+            }
+            Plant plant = await _context.Plants.Include(p => p.PlantImages)
+                .Include(p => p.PlantInformation).Include(p => p.PlantCategories).ThenInclude(p=>p.Category)
+                .FirstOrDefaultAsync(p=>p.Id == id);
+
+            List<Plant> plants = new List<Plant>();
+            List<Plant> plantsRange = new List<Plant>();
+            foreach (var item in plant.PlantCategories)
+            {
+                plants = _context.Plants.Where(x => x.PlantCategories.Any(z => z.CategoryId == item.CategoryId)).Include(x => x.PlantImages).ToList();
+
+                plantsRange.AddRange(plants);
+            }
+           
+            ViewBag.Plants = plantsRange.Distinct().ToList();
+
+            if (plant is null) return NotFound();
+            return View(plant);
+        }
+
+        public async Task<IActionResult> Partial()
+        {
+            List<Plant> plants = await _context.Plants.Include(p=>p.PlantImages).ToListAsync();
+
+            return PartialView("_PlantsPartialView", plants);
+
+        }
+
+        public async Task<IActionResult> GetDetail(int id)
+        {
+            Plant plant =await _context.Plants.Include(x => x.PlantImages).Include(x=>x.PlantInformation).FirstOrDefaultAsync(x => x.Id == id);
+            return PartialView("_detailplant",plant);
+        }
+
+        public async Task<IActionResult> AddBasket(int? id)
+        {
+            if (id is null || id == 0) return NotFound();
+
+            Plant plant = await _context.Plants.FirstOrDefaultAsync(p => p.Id == id);
+            if (plant == null) return NotFound();
+            string basketStr = HttpContext.Request.Cookies["Basket"];
+
+            BasketVM basket;
+
+            if (string.IsNullOrEmpty(basketStr))
+            {
+                basket = new BasketVM();
+                BasketCookieItemVM cookieItem = new BasketCookieItemVM
+                {
+                    Id = plant.Id,
+                    Quantity = 1
+                };
+                basket.BasketCookieItemVMs = new List<BasketCookieItemVM>();
+                basket.BasketCookieItemVMs.Add(cookieItem);
+                basket.TotalPrice = plant.Price;
+
+            }
+            else
+            {
+                basket = JsonConvert.DeserializeObject<BasketVM>(basketStr);
+                BasketCookieItemVM existed = basket.BasketCookieItemVMs.Find(p => p.Id == id);
+                if(existed == null)
+                {
+                    BasketCookieItemVM cookieItem = new BasketCookieItemVM
+                    {
+                        Id = plant.Id,
+                        Quantity = 1
+                    };
+                    basket.BasketCookieItemVMs.Add(cookieItem);
+                    basket.TotalPrice += plant.Price;
+                }
+                else
+                {
+                    basket.TotalPrice += plant.Price;
+                    existed.Quantity++;
+                }
+            }
+            basketStr = JsonConvert.SerializeObject(basket);
+
+            HttpContext.Response.Cookies.Append("Basket", basketStr);
+
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult ShowBasket()
+        {
+            if (HttpContext.Request.Cookies["Basket"] == null) return NotFound();
+            BasketVM basket = JsonConvert.DeserializeObject<BasketVM>(HttpContext.Request.Cookies["Basket"]);
+            return Json(basket);
+        }
+    }
+}
---
+++ P127_Pronia/P127_Pronia/Migrations/20220715052025_CreatedIsDatabase.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P127_Pronia.Migrations
+{
+    public partial class CreatedIsDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlantInformations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Shipping = table.Column<string>(nullable: true),
+                    AboutReturnRequest = table.Column<string>(nullable: true),
+                    Guarentee = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlantInformations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sliders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(nullable: true),
+                    Discount = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Article = table.Column<string>(nullable: true),
+                    ButtonUrl = table.Column<string>(nullable: true),
+                    Order = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sliders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(6,2)", nullable: false),
+                    Desc = table.Column<string>(nullable: true),
+                    SKU = table.Column<string>(nullable: true),
+                    PlantInformationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Plants_PlantInformations_PlantInformationId",
+                        column: x => x.PlantInformationId,
+                        principalTable: "PlantInformations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlantCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlantId = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlantCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlantCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlantCategories_Plants_PlantId",
+                        column: x => x.PlantId,
+                        principalTable: "Plants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlantImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Alternative = table.Column<string>(nullable: true),
+                    IsMain = table.Column<bool>(nullable: true),
+                    PlantId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlantImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlantImages_Plants_PlantId",
+                        column: x => x.PlantId,
+                        principalTable: "Plants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlantCategories_CategoryId",
+                table: "PlantCategories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlantCategories_PlantId",
+                table: "PlantCategories",
+                column: "PlantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlantImages_PlantId",
+                table: "PlantImages",
+                column: "PlantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plants_PlantInformationId",
+                table: "Plants",
+                column: "PlantInformationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Settings_Key",
+                table: "Settings",
+                column: "Key",
+                unique: true,
+                filter: "[Key] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlantCategories");
+
+            migrationBuilder.DropTable(
+                name: "PlantImages");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+
+            migrationBuilder.DropTable(
+                name: "Sliders");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Plants");
+
+            migrationBuilder.DropTable(
+                name: "PlantInformations");
+        }
+    }
+}
